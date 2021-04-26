24407c83aeb81a57e4aa5fa7c055907f176ca228
shuto3726 <sss3726mmm@gmail.com>
2021-04-26 04:23:55 +0000

    仕上げ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,60 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
+  def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render "index"
+    end
   end
   
   def edit
-    
-  end
-  
-  def show
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to book_path
+    end
   end
   
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
   
   def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "You have updated book successfully."
+    else
+       render "edit"
+    end
   end
   
-  def destoroy
-    book = Book.find(params[:id])
-    book.destoroy
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
+    
   end
+  
   private
-
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 
 end
