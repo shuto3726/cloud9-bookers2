6591061ae9510b05c23c6203d23c044aae1c14e6
shuto3726 <sss3726mmm@gmail.com>
2021-04-27 06:57:50 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,24 @@
 class BooksController < ApplicationController
+before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def show
     @newbook = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @newbook = Book.new(book_params)
+    @newbook.user_id = current_user.id
+    if @newbook.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book)
+      redirect_to book_path(@newbook)
     else
       @user = current_user
       @books = Book.all
       render "index"
     end
   end
-  
+
   def edit
     @book = Book.find(params[:id])
     if @book.user == current_user
@@ -26,13 +27,13 @@ class BooksController < ApplicationController
       redirect_to book_path
     end
   end
-  
+
   def index
     @user = current_user
-    @book = Book.new
+    @newbook = Book.new
     @books = Book.all
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
@@ -43,16 +44,23 @@ class BooksController < ApplicationController
        render "edit"
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
     flash[:notice] = "Book was successfully destroyed."
-    
+
   end
-  
+
   private
+    def ensure_correct_user
+      @book = Book.find(params[:id])
+      if @book.user_id != current_user.id
+        redirect_to books_path
+      end
+    end
+    
     def book_params
       params.require(:book).permit(:title, :body)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :ensure_correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -32,6 +33,13 @@ class UsersController < ApplicationController
   end
   
   private
+  def ensure_correct_user
+      @user = User.find(params[:id])
+      if @user != current_user
+        redirect_to user_path(current_user)
+      end
+  end
+  
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
